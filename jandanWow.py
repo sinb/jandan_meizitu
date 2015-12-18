623e7314a7be8c568332a1ea13bd30df23ce419a
sinb <linkin816@gmail.com>
2015-12-18 09:30:44 +0800

    无聊图也支持

--- jandanWow.py
+++ jandanWow.py
@@ -16,7 +16,7 @@ class jandanCrawler():
     def __init__(self):
         self.browser = mechanize.Browser()
         self.browser.set_handle_robots(False)
-        header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        header = {'User-Agent': 'Mozilla/5.8 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
        'Accept-Encoding': 'none',
@@ -97,8 +97,8 @@ if __name__ == '__main__':
 
     
     base_url = 'http://jandan.net/pic/page-'
-    start = 8000
-    end = 7900
+    start = 7899
+    end = 7600
     for page in range(start, end-1, -1):
         time.sleep(3)    
         url =  base_url + str(page)
